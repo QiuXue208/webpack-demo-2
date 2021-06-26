8a30ff4d51b19dee7f660104dfa49c620cd66eb6
Charles <1309571292@qq.com>
2021-06-26 19:03:16 +0800

    file to es5

---
+++ test.js
@@ -0,0 +1,5 @@
+let a = 'let';
+var b = 'b';
+const c = 5;
+
+const fn = (a, b, c) => a + b + c;
---
+++ test_es5.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var a = 'let';
+var b = 'b';
+var c = 5;
+
+var fn = function fn(a, b, c) {
+  return a + b + c;
+};
